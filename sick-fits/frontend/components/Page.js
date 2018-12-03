623b460b1d750815e2d76746f832bf43bbbbc043
Ivangzzm94 <ivanmontemayor94@gmail.com>
2018-12-03 15:28:46 -0600

    reser git user

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+import styled, {ThemeProvider, injectGlobal} from 'styled-components';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const StyledPage = styled.div`
+    backgorund: withTheme;
+    color: black;
+`;
+
+const Inner = styled.div`
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 2rem;
+`;
 
 class Page extends Component {
     render() {
         return (
-            <div>
-                <Header />
-                {this.props.children}
-            </div>
+            <ThemeProvider theme={theme}>
+                <StyledPage>
+                    <Meta />
+                    <Header />
+                    <Inner>{this.props.children}</Inner>
+                </StyledPage>
+            </ThemeProvider>
         );
     }
 }
