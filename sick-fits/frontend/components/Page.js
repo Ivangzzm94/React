3252afd788cc431d5ace5acae3730a152b5a5127
dcoello <d.coello@bmtask.com>
2018-12-03 13:21:22 -0600

    first commit

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Meta from '../components/Meta';
+
+class Page extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default Page;
