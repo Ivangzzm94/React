406bd44137b0a3fb1c58d688683567f1aef3f150
Ivangzzm94 <ivanmontemayor94@gmail.com>
2018-12-14 12:31:22 -0600

    module 5  lesson 35: Updating Permissions on the Server

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const { transport, makeANiceEmail } = require('../mail');
+const { hasPermission } = require('../utils');
 
 const Mutations = {
     async createItem(parent, args, ctx, info) {
@@ -139,7 +140,7 @@ const Mutations = {
         //4. Return a message
         return { message: 'Thanks!'};
     },
-    async resetPassword(parent, args, ctx, indo) {
+    async resetPassword(parent, args, ctx, info) {
         //1. Check if the passwords match
         if(args.password !== args.confirmPassword) {
             throw new Error('You Password dont match!');
@@ -175,7 +176,37 @@ const Mutations = {
         });
         //8. Return the new user
         return updatedUser;
-    }
+    },
+    async updatePermissions(parent, args, ctx, info) {
+        //1. Check if they are logged in
+        if(!ctx.request.userId) {
+            throw new Error('You must be logged in!');
+        }
+        //2. Query the current user
+        const currentUser = await ctx.db.query.user(
+            {
+                where: {
+                    id: ctx.request.userId,
+                },
+            },
+            info
+        );
+        //3. Check if they have permission to do this
+        hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE']);
+        //4. Update the permissions
+        return ctx.db.mutation.updateUser({
+            data: {
+                permissions: {
+                    set: args.permissions,
+                },
+            },
+            where: {
+                id: args.userId,
+            },
+        }, 
+        info
+        );
+    },
 };
 
 module.exports = Mutations;
--- sick-fits/frontend/components/Permissions.js
+++ sick-fits/frontend/components/Permissions.js
@@ -1,4 +1,4 @@
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import Error from './ErrorMessage';
 import gql from 'graphql-tag';
 import Table from './styles/Table';
@@ -14,6 +14,17 @@ const possiblePermissions = [
     'PERMISSIONUPDATE',
 ];
 
+const UPDATE_PERMISSIONS_MUTATION = gql`
+  mutation updatePermissions($permissions: [Permission], $userId: ID!) {
+    updatePermissions(permissions: $permissions, userId: $userId) {
+      id
+      permissions
+      name
+      email
+    }
+  }
+`;
+
 const ALL_USERS_QUERY = gql`
 query {
     users {
@@ -26,29 +37,28 @@ query {
 `;
 
 const Permissions = props => (
-<Query query={ALL_USERS_QUERY}>
-    {({ data, loading, error }) => (
+    <Query query={ALL_USERS_QUERY}>
+      {({ data, loading, error }) => (
         <div>
-            <Error error={error} />
-            <div>
-                <h2>Manage Permissions</h2>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            {possiblePermissions.map(permission => <th key={permission}>{permission}</th>)}
-                            <th>√</th>
-                        </tr>
-                    </thead>
-                    <tbody>{data.users.map(user => <UserPermissions user={user} key={user.id} />)}
-                    </tbody>
-                </Table>
-            </div>
+          <Error error={error} />
+          <div>
+            <h2>Manage Permissions</h2>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  {possiblePermissions.map(permission => <th key={permission}>{permission}</th>)}
+                  <th>👇🏻</th>
+                </tr>
+              </thead>
+              <tbody>{data.users.map(user => <UserPermissions user={user} key={user.id} />)}</tbody>
+            </Table>
+          </div>
         </div>
-    )}
-</Query>
-);
+      )}
+    </Query>
+  );
 
 class UserPermissions extends React.Component {
     static propTypes = {
@@ -85,27 +95,44 @@ class UserPermissions extends React.Component {
     render() {
         const user = this.props.user;
         return (
-            <tr>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            {possiblePermissions.map(permission => (
+        <Mutation mutation={UPDATE_PERMISSIONS_MUTATION} variables={{
+            permissions: this.state.permissions,
+            userId: this.props.user.id
+            }}
+        >
+            {(updatedPermissions, {loading, error}) => (
+                <>
+            {error && <tr><td colspan="8"><Error error={error} /></td></tr>}
+            < tr >
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              {possiblePermissions.map(permission => (
                 <td key={permission}>
-                <label htmlFor={`${user.id}-permission-${permission}`}>
-                    <input 
-                    type="checkbox" 
-                    checked={this.state.permissions.includes(permission)}
-                    value={permission}
-                    onChange={this.handlePermissionChange} />
-                </label>
+                  <label htmlFor={`${user.id}-permission-${permission}`}>
+                    <input
+                      id={`${user.id}-permission-${permission}`}
+                      type="checkbox"
+                      checked={this.state.permissions.includes(permission)}
+                      value={permission}
+                      onChange={this.handlePermissionChange}
+                    />
+                  </label>
                 </td>
-            ))}
-            <td>
-                <SickButton>Update</SickButton>
-            </td>
+              ))}
+              <td>
+                <SickButton type="button" disabled={loading} onClick={updatedPermissions}>
+                  Updat{loading ? 'ing' : 'e'}
+                </SickButton>
+              </td>
             </tr>
-        );
-    }
+          </>
+        )
+        }
+      </Mutation>
+    );
+  }
 }
+
 
 
 export default Permissions;
