7431c4e4ab8e25801eac572b08653f93f69c8a72
Ivangzzm94 <ivanmontemayor94@gmail.com>
2018-12-20 12:09:57 -0600

    module 6 lesson 42: Removing Cart Items

--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import formatMoney from '../lib/formatMoney';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
     padding: 1rem 0;
@@ -30,6 +31,7 @@ const CartItem = ({ cartItem }) => <CartItemStyles>
     </em>
     </p>
 </div>
+<RemoveFromCart id={cartItem.id} />
 </CartItemStyles>
 
 CartItem.propTypes = {
